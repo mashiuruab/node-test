429d2b9511978184d6ed219d87d136c9be3e6a3f
mashiur <mashiur@uab.com>
2017-10-25 22:06:46 -0500

    jshint and jslint now working properly in the node applicaiton.
    - some minor refactoring  done.

---
+++ server_jshint.js
@@ -0,0 +1,101 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var jshint = require('jshint').JSHINT;
+
+var bodyParser = require('body-parser');
+var multer  = require('multer');
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/'}).any());
+
+/*'file' this should be key  from HTTP client
+curl -X POST -F "file=@test.js" -F "file=@test.js" http://localhost:8081/upload/
+* */
+
+app.post('/upload', function (request, response) {
+    var filePath = __dirname + "/" + request.files[0].filename;
+
+    fs.readFile( request.files[0].path, function (err, data) {
+        if (err) {
+            console.log("Error On Reading file %s", request.files[0].path);
+            console.log(err);
+            return;
+        }
+
+        fs.writeFile(filePath, data, function (err) {
+            if( err ){
+                console.log("Error on writing file at path : %s", filePath);
+                console.log( err );
+                response = {
+                    message:'Error Happened',
+                    error:err
+                };
+
+                response.end( JSON.stringify( response ) );
+                return;
+            }
+
+
+            try {
+                checkJsHint(filePath, response);
+            } catch (err) {
+                console.log(err);
+            } finally {
+                removeFileSync(filePath);
+            }
+        });
+    });
+})
+
+
+function removeFileSync(filePath) {
+    const fs = require('fs');
+
+    fs.unlinkSync(filePath);
+    console.log('successfully deleted %s', filePath);
+}
+
+function readFileSync(filePath) {
+    var fileString = fs.readFileSync(filePath, "utf8");
+    return fileString;
+}
+
+function checkJsHint(filePath, response) {
+    var fileContent = readFileSync(filePath);
+
+    var source = [
+        fileContent
+    ];
+    var options = {
+        undef: true,
+        /*'-W100': true,
+        '-W097': true*/
+    };
+    var predef = {
+        foo: false
+    };
+
+    jshint(source, options, predef)
+
+    if (jshint.errors && jshint.errors.length > 0) {
+        console.log(jshint.errors);
+        var respObj = {
+            status : 'error',
+            error  : jshint.errors
+        };
+        response.end( JSON.stringify(respObj));
+    } else {
+        var respObj = {
+            status : "ok"
+        }
+
+        response.end( JSON.stringify(respObj));
+    }
+}
+
+var server = app.listen(8081, function () {
+    console.log("Server started");
+    console.log(server.address());
+})
